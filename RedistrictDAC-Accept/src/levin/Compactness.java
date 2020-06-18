baf61303210911541d2eb5b8a6c326142b2a1c48
Marc Chmielewski <54959001+blucifer22@users.noreply.github.com>
2020-06-18 11:52:03 -0400

    Documentation

--- RedistrictDAC-Accept/src/levin/Compactness.java
+++ RedistrictDAC-Accept/src/levin/Compactness.java
@@ -11,7 +11,10 @@ import levin.circle.SECPoint;
 import levin.circle.Smallestenclosingcircle;
 import levin.compactnessScore.HaversineDistance;
 
-
+/** Manages several measures of compactness and the dynamic calculation thereof
+ * @author Harry Levin (presumably)
+ *
+ */
 public class Compactness {
     private District district;
     private double area;
@@ -20,23 +23,40 @@ public class Compactness {
     private double smallestCircleArea;
     private double smallestCirclePerim;
 
+    /**
+     * Calculate several key compactness metrics and store them as class variables.
+     * @param d The District that we're measuring
+     */
     public Compactness(District d) {
         this.district = d;
-        Geometry districtGeom = d.getRealGeometry();
-        this.area = districtGeom.getArea();
-        this.perimeter = districtGeom.getLength();
-        this.convexHull = districtGeom.convexHull();
+        Geometry districtGeom = d.getRealGeometry(); // Gets the geometry of the district (Real geometry may be redundant?)
+        this.area = districtGeom.getArea(); // Get area
+        this.perimeter = districtGeom.getLength(); // Get perimeter
+        this.convexHull = districtGeom.convexHull(); // Calculate the convex hull
     }
 
+    /**
+     * Calculates the Convex Hull Ratio measure of compactness.
+     * The Convex Hull Ratio is defined as the area of the district
+     * divided by the area of the convex hull of the same district.
+     * @return The Convex Hull Ratio compactness measure of the district.
+     */
     public double getConvexHullMeasure() {
-        double result = this.district.getGeometry().getArea() / this.convexHull.getArea();
-        if (!this.validate(result)) {
-            System.err.println("Result out of bounds: " + result);
+        double result = this.district.getGeometry().getArea() / this.convexHull.getArea(); // Calculates the Convex Hull Ratio
+        if (!this.validate(result)) { // Sanity check the result
+            System.err.println("Result out of bounds: " + result); // Panic if we break math
             System.exit(0);
         }
-        return result;
+        return result; // Return the result
     }
 
+    /**
+     * Calculates the Reock Compactness Ratio (ultimately unused in the paper, presumably left behind as legacy?)
+     * The Reock Compactness Ratio is the ratio of a District's area to that of the minimum bounding circle that
+     * surrounds it. The scale is self normalizing from 0 to 1, with 0 indicating minimally compact and 1 being most
+     * compact.
+     * @return The Reock Compactness Ratio for the provided District
+     */
     public double getReockMeasure() {
         double result = this.district.getGeometry().getArea() / this.smallestCircleArea;
         if (!this.validate(result)) {
@@ -46,6 +66,12 @@ public class Compactness {
         return result;
     }
 
+    /**
+     * Calculate the Polsby-Popper Measure of district compactness.
+     * The Polsby-Popper Ratio is a measure of the ratio between the area of a District
+     * and the area of a circle whose circumference is the same as the District's area.
+     * @return The Polsby-Popper Measure of compactness for the district.
+     */
     public double getPolsbyPopperMeasure() {
         double districtPerim = this.district.getGeometry().getLength();
         double radius = districtPerim / 6.283185307179586;
@@ -58,6 +84,12 @@ public class Compactness {
         return result;
     }
 
+    /**
+     * Calculate the Modified Schwartzberg Measure of district compactness.
+     * The Modified Schwartzberg Ratio is a measure of the ratio between the perimeter of a circle with
+     * the same area as the district and the perimeter of the actual district.
+     * @return Modified Schwartzberg Ratio of compactness for the district.
+     */
     public double getModifiedSchwartzberg() {
         double districtArea = this.district.getGeometry().getArea();
         double radius = Math.sqrt(districtArea / 3.141592653589793);
@@ -70,6 +102,13 @@ public class Compactness {
         return result;
     }
 
+    /**
+     * A simple validation check for the various measures of compactness.
+     * Since all of the measures are normed from 0 to 1, if a measure exceeds that
+     * range there must be an upstream/implementation error.
+     * @param result The value of the measure of compactness in question.
+     * @return Whether or not the result is within the range of 0 to 1 and thus, valid
+     */
     private boolean validate(double result) {
         if (result >= 0.0 && result <= 1.0) {
             return true;
@@ -77,6 +116,13 @@ public class Compactness {
         return false;
     }
 
+    /**
+     * Calculate the radius of the smallest enclosing circle of a given District.
+     * NOTE: The Reock measure seems to have been thrown out for some reason (possibly complexity?),
+     * thus this and its upstream libraries were not used for much of anything.
+     * @param d The District in question
+     * @return The radius of the smallest enclosing circle for the District in question.
+     */
     public double getDistrictSmallestEnclosingCircleRadius(District d) {
         List<SECPoint> points = this.getPointsForGeometry(d.getGeometry());
         Smallestenclosingcircle circle = new Smallestenclosingcircle();
@@ -92,6 +138,11 @@ public class Compactness {
         return actualRadius;
     }
 
+    /**
+     * Converts between "standard" Geometry points and SECPoints
+     * @param geom A Geometry object
+     * @return A list of SECPoints equivalent to the constituent points of the inputted geometry
+     */
     public List<SECPoint> getPointsForGeometry(Geometry geom) {
         Coordinate[] coordinates = geom.getCoordinates();
         System.out.println("Num Coordinates: " + coordinates.length);
